d944e9a598718117f8a0c41dfbe9328b08f78fd7
ktotokto <nazarsilovskij79@gmail.com>
2025-02-27 01:36:58 +0300

    Часть № 3

--- main.py
+++ main.py
@@ -9,27 +9,35 @@ BLACK = pygame.Color("#000000")
 
 
 class MapParams:
-    def __init__(self, coordinates, z_index, apikey):
+    def __init__(self, coordinates, z_index, apikey, coefficient):
         self.coordinates, self.z_index, self.apikey = coordinates.split(), z_index, apikey
+        self.coefficient = coefficient
+        self.key_list = [pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
         self.get_map()
 
     def key_event(self, key):
-        if key == pygame.K_PAGEUP:
-            new_z_index = (int(self.z_index) - 1)
-            self.z_index = str(new_z_index) if new_z_index >= 1 else "1"
-        elif key == pygame.K_PAGEDOWN:
-            new_z_index = (int(self.z_index) + 1)
-            self.z_index = str(new_z_index) if new_z_index <= 21 else "21"
-        elif key == pygame.K_UP:
-            pass
-            # self.coordinates[1] = str(float(self.coordinates[1]) + 10 / (int(self.z_index) * 2))
-        elif key == pygame.K_DOWN:
-            pass
-            # self.coordinates[1] = str(float(self.coordinates[1]) - 10 / (int(self.z_index) * 2))
-        elif key == pygame.K_LEFT:
-            pass
-        elif key == pygame.K_RIGHT:
-            pass
+        if key in self.key_list:
+            if key == self.key_list[0]:
+                new_z_index = (int(self.z_index) - 1)
+                if new_z_index >= 1:
+                    self.coefficient, self.z_index = self.coefficient * 2, str(new_z_index)
+            elif key == self.key_list[1]:
+                new_z_index = (int(self.z_index) + 1)
+                if new_z_index <= 21:
+                    self.coefficient, self.z_index = self.coefficient / 2, str(new_z_index)
+            elif key == self.key_list[2]:
+                self.coordinates[1] = str(float(self.coordinates[1]) + self.coefficient)
+            elif key == self.key_list[3]:
+                self.coordinates[1] = str(float(self.coordinates[1]) - self.coefficient)
+            elif key == self.key_list[4]:
+                self.coordinates[0] = str(float(self.coordinates[0]) - self.coefficient * 2)
+            elif key == self.key_list[5]:
+                self.coordinates[0] = str(float(self.coordinates[0]) + self.coefficient * 2)
+            coord_0, coord_1 = float(self.coordinates[0]), float(self.coordinates[1])
+            self.coordinates[0] = str(-(coord_0 / abs(coord_0)) * (360.0 - abs(coord_0))) if abs(coord_0) > 180.0 else self.coordinates[0]
+            self.coordinates[1] = str((coord_1 / abs(coord_1)) * 85.0) if abs(coord_1) > 85.0 else self.coordinates[1]
+            self.get_map()
+        print(self.coordinates)
 
     def get_map(self):
         map_response = get_map_response(self.coordinates, self.z_index, self.apikey)
@@ -72,7 +80,7 @@ def get_map_response(coordinates, z_index, apikey):
 
 
 json_response = get_json_response("Сыктывкар", "2cba1e40-eafb-42e1-8e61-539727bb58a2")
-map_params = MapParams(get_coordinates(json_response), "12", "0720951d-bde7-4048-8e6c-f22b5f5c3301")
+map_params = MapParams(get_coordinates(json_response), "1", "0720951d-bde7-4048-8e6c-f22b5f5c3301", 40)
 
 pygame.init()
 pygame.display.set_caption("Карта (не историческая)")
@@ -84,6 +92,6 @@ while True:
         if event.type == pygame.KEYDOWN:
             map_params.key_event(event.key)
     screen.fill(BLACK)
-    img = map_params.get_map()
+    img = map_params.map
     screen.blit(img, img.get_rect(center=(W // 2, H // 2)))
     pygame.display.flip()
